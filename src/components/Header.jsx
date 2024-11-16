f810969abe95fd7a1020b4486914adba8e36548c
Ace-Aryal <aryaldeelep@gmail.com>
2024-11-16 16:42:07 +0545

    1stCommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+ function Header (){
+
+    return(
+        <nav className="nav">
+            <h1 className="headingText">Keepr</h1>
+        </nav>
+    )
+ }
+
+ export default Header
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+
+function Note (){
+
+    return(
+      <div className="note-container">
+        <h2 className="noteHeading">This is a Note Title</h2>
+        <p className="noteContent">This is a note content </p>
+      </div>
+    )
+ }
+
+ export default Note

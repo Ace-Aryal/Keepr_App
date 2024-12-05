e6c98eb7b8673086ebce31e5ce54df5d2cce473d
Dipesh Aryal <153974691+Ace-Aryal@users.noreply.github.com>
2024-12-05 12:11:33 +0000

    hello

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,31 +1,63 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Note from "./Note";
 function Input(props) {
-  const [notes, setNotes] = useState([]);
-  function AddNotes() {
-    let newNote = {
-      id: notes.length,
-      title: ,
-    };
-    setNotes((currentNotes) => {
-      return currentNotes.push(newNote);
-    });
+  const [titleText, setTitleText] = useState("");
+  const [contentText, setContentText] = useState("");
+  const [notes, setNotes] = useState([
+    {
+      title: "hi",
+      content: "i love you",
+    },
+  ]);
+  function handleChange(e) {
+    e.preventDefault();
+    const { value, name } = e.target;
+    name === "title" ? setTitleText(value) : setContentText(value);
   }
 
+  function AddNotes(e) {
+    e.preventDefault();
+    setNotes((prevValue) => {
+      return {
+        ({
+        ...prevValue,
+        },
+        {
+        title: titleText,
+        content: contentText,
+        
+        })
+      };
+    });
+  }
   return (
-    <section className="top">
-      <form className="input-container">
-        <input type="text" name="title" id="input-title" placeholder=" Title" />{" "}
-        <br />
-        <textarea
-          type="text"
-          name="content"
-          id="input-content"
-          placeholder="Add a note... "
-        ></textarea>
-        <button>Add Note</button>
-      </form>
-    </section>
+    <>
+      <section className="top">
+        <form className="input-container">
+          <input
+            type="text"
+            name="title"
+            id="input-title"
+            placeholder=" Title"
+            value={titleText}
+            onChange={handleChange}
+          />{" "}
+          <br />
+          <textarea
+            type="text"
+            name="content"
+            id="input-content"
+            onChange={handleChange}
+            value={contentText}
+            placeholder="Add a note... "
+          ></textarea>
+          <button onClick={AddNotes}>Add Note</button>
+        </form>
+      </section>
+      {notes.map((note) => {
+        <Note title={note.title} content={note.title} />;
+      })}
+    </>
   );
 }
 
